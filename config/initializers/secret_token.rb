df7e15a40eba2a041ec9fcddf3d1606b9af57074
Bryan Wilber <bryan@bryan-desktop.(none)>
2011-06-03 21:28:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Henryapp1::Application.config.secret_token = '4228a5adbe8ab5c0b9f8a1815a92ebf64631a96cc4956a164bc48bbcbc2c02de6c12201203ebd3ace62b05c27d39225a949e8f6cc7806d27ada2406878ab470c'
